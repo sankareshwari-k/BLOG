3739e20289dbb22cc7748535a03ebbab77aa72ef
sankareshwari-k <sankareshwarisankari@gmail.com>
2025-06-22 17:30:10 +0530

    Project Completed

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
+import BlogList from '../components/BlogList.jsx'
+import Newsletter from '../components/Newsletter.jsx'
+import Footer from '../components/Footer.jsx'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <BlogList/>
+      <Newsletter/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
