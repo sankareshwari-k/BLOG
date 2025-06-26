80ab5e1379dcd8777b61653e102f2e0df6b23c04
sankareshwari-k <sankareshwarisankari@gmail.com>
2025-06-26 19:42:59 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Ensure this points to the right folder
+  build: {
+    outDir: 'dist'
+  }
 })
